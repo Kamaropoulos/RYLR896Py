ae7d98449b08d8ffa366494db98d924e83321b08
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2020-01-05 15:54:35 +0200

    feat: Create RYLR896 class and method placeholders

---
+++ RYLR896Py/rylr896.py
@@ -0,0 +1,59 @@
+# import serial
+
+class RYLR896():
+    ser = None
+    def __init__(self, tty=None, baudrate=None):
+        # self.ser = serial.Serial(tty, baudrate, timeout=1)
+        # TODO: Set IPR
+        pass
+
+    def Test(self):
+        pass
+
+    def Reset(self):
+        pass
+
+    def Send(self, message, address=None):
+        if address is None:
+            # Broadcast
+            pass
+        else:
+            # Send to address
+            pass
+
+    def Receive(self):
+        pass
+
+    def SleepMode(self):
+        pass
+
+    def NormalMode(self):
+        pass
+
+    def SetRFParams(self, spreadingFactor, bandwidth, codingRate, programmedPreamble):
+        pass
+
+    def SetRFParamsLessThan3KM(self):
+        pass
+
+    def SetRFParamsMoreThan3KM(self):
+        pass
+
+    def SetAddress(self, address):
+        pass
+
+    def GetAddress(self):
+        pass
+
+    def SetNetworkID(self, networkID):
+        pass
+
+    def __WriteToLoRa(self, message):
+        pass
+
+    def __ReadFromLoRa(self):
+        pass
+
+    def __SetIPR(self, IPR):
+        pass
+
